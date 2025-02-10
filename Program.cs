a03a525585fc21335a7324174512458f16e7e47a
Elyes Bouzaiene <elyes.bouzaiene@avanade.com>
2025-02-10 16:53:08 +0100

    Perfect numbers calculation Parallel processing activated

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Hello, please enter a natural number:");
+string value = Console.ReadLine()!;
+
+Stopwatch stopWatch = new();
+stopWatch.Start();
+
+Console.WriteLine("Start Processing...");
+
+int max = int.Parse(value);
+
+Parallel.For(0, max, (number) =>
+{
+    if (Perfect(number))
+        Console.WriteLine($"{number} is perfect.");
+});
+
+//for (int number = 0; number <= max; number++)
+//{
+//    if (Perfect(number))
+//        Console.WriteLine($"{number} is perfect.");
+//}
+
+
+static bool Perfect(int num)
+{
+    int i = 1;
+    int divisorSum = 0;
+    while (i < (num / 2) + 1)
+    {
+        if (num % i == 0)
+            divisorSum += i;
+
+        i++;
+    }
+
+    return divisorSum == num;
+}
+
+TimeSpan ts = stopWatch.Elapsed;
+
+// Format and display the TimeSpan value.
+string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+    ts.Hours, ts.Minutes, ts.Seconds,
+    ts.Milliseconds / 10);
+
+Console.WriteLine($"End Processing. Duration: {elapsedTime}");
+Console.ReadKey();
